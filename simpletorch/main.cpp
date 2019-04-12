88f859d9f936e0e087318492240a9c21fb746f17
lynnazhou <smilekevinfs@gmail.com>
2019-04-12 15:52:28 +0800

    add example to create graded tensor

--- simpletorch/main.cpp
+++ simpletorch/main.cpp
@@ -3,7 +3,11 @@
 #include <torch/extension.h>
 
 int main(int argc, char* argv[]) {
-    torch::Tensor a = torch::ones({2, 2});
+    torch::TensorOptions option = torch::TensorOptions()
+                                    .dtype(torch::kFloat32)
+                                    .layout(torch::kStrided)
+                                    .requires_grad(true);
+    torch::Tensor a = torch::ones({2, 2}, option);
     torch::Tensor b = torch::randn({2, 2});
 
     auto c = a + b;
